d83f779d89b1062ff9987c31a78fb90f8f0dd331
frankl <frank@luebecknet.de>
2015-03-09 01:16:46 +0100

    long options and better error messages

--- src/shmcat.c
+++ src/shmcat.c
@@ -3,15 +3,16 @@
     gcc -Wall -O0 -o shmcat shmcat.c -lrt
 */
 
+#include "version.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <getopt.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
-#include "version.h"
 #include "cprefresh.h"
 
 void usage( ) {
@@ -21,10 +22,13 @@ void usage( ) {
   fprintf(stderr, ")\nUSAGE:\n");
   fprintf(stderr,
 "\n"
-" shmcat memname blksize\n"
+" shmcat --shmname=memname [--block-size=blksize]\n"
+" shmcat -i memname [-b blksize]\n"
 "\n"
 "  This program writes the content of a shared memory file memname in blocks\n"
-"  of size blksize to stdout. The shared memory file is deleted afterwards.\n"
+"  of  blksize bytes to stdout. The shared memory file is deleted afterwards.\n"
+"\n"
+"  The default for blksize is 8192.\n"
 "\n"
 "  You may create the shared memory file with 'cptoshm'.\n"
 "\n"
@@ -34,30 +38,62 @@ void usage( ) {
 int main(int argc, char *argv[])
 {
   char *memname;
-  int fd;
+  int fd, optc;
   size_t length, blen, done, wlen;
   struct stat sb;
   char *mem, *ptr;
 
-  if (argc != 3) {
+  /* read command line options */
+  static struct option longoptions[] = {
+      {"shmname", required_argument, 0, 'i' },
+      {"block-size", required_argument, 0,  'b' },
+      {"version", no_argument, 0, 'V' },
+      {"help", no_argument, 0, 'h' },
+      {0,         0,                 0,  0 }
+  };
+
+  if (argc == 1) {
      usage();
      exit(0);
   }
+  blen = 8192;
+  memname = NULL;
+  while ((optc = getopt_long(argc, argv, "i:b:Vh",
+          longoptions, &optind)) != -1) {
+      switch (optc) {
+      case 'i':
+        memname = optarg;
+        break;
+      case 'b':
+        blen = atoi(optarg);
+        break;
+      case 'V':
+        fprintf(stderr,
+                "cptoshm (version %s of frankl's stereo utilities)\n",
+                VERSION);
+        exit(0);
+      default:
+        usage();
+        exit(0);
+      }
+  }
 
-  memname = argv[1];
-  blen = atoi(argv[2]);
+  if (memname == NULL) {
+      fprintf(stderr, "shmcat: need --shmname argument . . . bye.\n");
+      exit(7);
+  }
   if ((fd = shm_open(memname, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1){
-      fprintf(stderr, "Cannot open memory %s.\n", memname);
+      fprintf(stderr, "shmcat: Cannot open memory %s.\n", memname);
       exit(3);
   }
   if (fstat(fd, &sb) == -1) {
-      fprintf(stderr, "Cannot stat shared memory %s.\n", memname);
+      fprintf(stderr, "shmcat: Cannot stat shared memory %s.\n", memname);
       exit(4);
   }
   length = sb.st_size;
   mem = mmap(NULL, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (mem == MAP_FAILED) {
-      fprintf(stderr, "Cannot map shared memory.");
+      fprintf(stderr, "shmcat: Cannot map shared memory.\n");
       exit(6);
   }
   ptr = mem;
@@ -75,7 +111,7 @@ int main(int argc, char *argv[])
       ptr += wlen;
   }
   if (shm_unlink(memname) == -1) {
-      fprintf(stderr, "Cannot unlink shared memory.");
+      fprintf(stderr, "shmcat: Cannot unlink shared memory.\n");
       exit(7);
   }
   exit(0);
